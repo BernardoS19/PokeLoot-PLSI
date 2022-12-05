6db75b1a68034e72250bf613b8f27be4772ec272
Bernardo Santos <2190723@my.ipleiria.com>
2022-12-05 16:43:55 +0000

    users separados por roles no index

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -62,11 +62,13 @@ class UserController extends Controller
         }
 
         $searchModel = new UserSearch();
-        $dataProvider = $searchModel->search($this->request->queryParams);
+        $dataProvider = $searchModel->search($this->request->queryParams, ['admin', 'avaliador']);
+        $dataProviderCliente = $searchModel->search($this->request->queryParams, 'cliente');
 
         return $this->render('index', [
             'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
+            'dataProviderCliente' => $dataProviderCliente,
         ]);
     }
 
