057f5de8463fb6992902ee58c949ef8e8a3abb7a
Ivo Duarte <mulalha.duarte@gmail.com>
2022-12-06 13:30:56 +0000

    vista remover lista desejos

--- frontend/views/lista_desejos/index.php
+++ frontend/views/lista_desejos/index.php
@@ -35,6 +35,9 @@ $this->title = 'Lista de Desejos';
                                     <a href="" class="social-info">
                                         <span><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
                                     </a>
+
+                                       <span></span><?= \yii\bootstrap5\Html::a('<i class="fa fa-star"></i>',['lista_desejos/remover?cartaId='.$item->carta_id], ['data'=>['method'=>'post'],'class'=>'icon_btn']) ?>
+
                                 </div>
                             </div>
                         </div>
