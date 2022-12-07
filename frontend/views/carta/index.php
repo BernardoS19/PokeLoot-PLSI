32601e3443e69cede91d5f14dc8d353d00363bfd
joaopatroc <2190740@my.ipleiria.pt>
2022-12-07 14:29:13 +0000

    adicionar wishlist(continuação)

--- frontend/views/carta/index.php
+++ frontend/views/carta/index.php
@@ -219,23 +219,10 @@ $this->title = 'Catálogo';
                                     <h6><?= $carta->preco ?> €</h6>
                                 </div>
                                 <div class="prd-bottom">
-
-                                    <a href="" class="social-info">
-                                        <span class="ti-bag"></span>
-                                        <p class="hover-text">add to bag</p>
-                                    </a>
-                                    <a href="" class="social-info">
-                                        <span class="lnr lnr-heart"></span>
-                                        <p class="hover-text">Wishlist</p>
-                                    </a>
-                                    <a href="" class="social-info">
-                                        <span class="lnr lnr-sync"></span>
-                                        <p class="hover-text">compare</p>
-                                    </a>
-                                    <a href="" class="social-info">
-                                        <span class="lnr lnr-move"></span>
-                                        <p class="hover-text">view more</p>
-                                    </a>
+                                    <div class="card_area d-flex align-items-center">
+                                        <a class="primary-btn" href="#">Add to Cart</a>
+                                        <?= \yii\bootstrap5\Html::a('<i class="fa fa-star"></i>',['lista_desejos/adicionar?cartaId='.$carta->id], ['data'=>['method'=>'post'],'class'=>'icon_btn']) ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
