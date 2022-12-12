b2dc55b3a2d2b5fa2c4258f7fcea13ef091df666
Ivo Duarte <mulalha.duarte@gmail.com>
2022-12-12 16:41:58 +0000

    update botão-Carrinho/lista-desejos

--- frontend/views/carta/index.php
+++ frontend/views/carta/index.php
@@ -219,23 +219,12 @@ $this->title = 'Catálogo';
                                     <h6><?= $carta->preco ?> €</h6>
                                 </div>
                                 <div class="prd-bottom">
-
-                                    <a href="" class="social-info">
-                                        <span class="ti-bag"></span>
-                                        <p class="hover-text">add to bag</p>
-                                    </a>
-                                    <a href="" class="social-info">
-                                        <span class="lnr lnr-heart"></span>
-                                        <p class="hover-text">Wishlist</p>
-                                    </a>
-                                    <a href="" class="social-info">
-                                        <span class="lnr lnr-sync"></span>
-                                        <p class="hover-text">compare</p>
-                                    </a>
-                                    <a href="" class="social-info">
-                                        <span class="lnr lnr-move"></span>
-                                        <p class="hover-text">view more</p>
-                                    </a>
+                                    <div class="card_area d-flex align-items-center">
+                                        <a href="" class="social-info">
+                                            <span><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
+                                        </a>
+                                        <?= \yii\bootstrap5\Html::a('<span><i class="fa fa-star"></i></span>',['lista_desejos/adicionar?cartaId='.$carta->id], ['data'=>['method'=>'post'],'class'=>'social-info']) ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
