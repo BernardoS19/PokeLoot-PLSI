0c90c9a9f645876e74c19b0115e0c0750ceec8dc
Bernardo Santos <2190723@my.ipleiria.pt>
2023-01-20 01:45:36 +0000

    criar baralho

--- backend/modules/api/controllers/BaralhoController.php
+++ backend/modules/api/controllers/BaralhoController.php
@@ -5,6 +5,7 @@ namespace backend\modules\api\controllers;
 use common\models\Baralho;
 use common\models\User;
 use Yii;
+use yii\filters\auth\AuthInterface;
 use yii\rest\ActiveController;
 
 class BaralhoController extends ActiveController
@@ -27,6 +28,35 @@ class BaralhoController extends ActiveController
                 array_push($baralhos, $myObj);
             }
             return $baralhos;
+        }
+        else {
+            $myObj = new \stdClass();
+            $myObj->error = "Erro. Utilizador não existe.";
+            return $myObj;
+        }
+    }
+
+    public function actionNovo()
+    {
+        $authkey = Yii::$app->request->headers["auth"];
+
+        if (User::findByAuthKey($authkey)) {
+            $user = User::findByAuthKey($authkey);
+
+            $baralho = new Baralho();
+            $baralho->nome = $this->request->post("nome");
+            $baralho->user_id = $user->id;
+
+            if ($baralho->save()) {
+                $myObj = new \stdClass();
+                $myObj->status = "Baralho criado com sucesso";
+                return $myObj;
+            }
+            else {
+                $myObj = new \stdClass();
+                $myObj->status = $baralho->errors;
+                return $myObj;
+            }
         }
         else {
             $myObj = new \stdClass();
